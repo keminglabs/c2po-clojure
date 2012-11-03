d9ae14763a1bba9e6ee8c31e4c4f0d825c8d901f
Kevin J. Lynagh <kevin@keminglabs.com>
2012-11-02 23:24:04 -0700

    Don't print tagged literal within a string.

--- src/clj/c2po/literals.clj
+++ src/clj/c2po/literals.clj
@@ -3,4 +3,4 @@
 (def c2po-literal-prefix "com.keminglabs/c2po")
 
 (defn print-literal [prefix name x writer]
-  (.write writer (str "\"#" c2po-literal-prefix "." prefix "." name " " (pr-str (into {} x)) "\"")))
+  (.write writer (str "#" c2po-literal-prefix "." prefix "." name " " (pr-str (into {} x)))))
