deed5f05044ca18a8aa8bca4dba502654154510b
Kevin J. Lynagh <kevin@keminglabs.com>
2012-11-29 21:02:00 -0800

    Update usage example.

--- examples/1-usage.clj
+++ examples/1-usage.clj
@@ -1,10 +1,11 @@
 (ns c2po.examples.usage
   (:require [c2po.core :refer [c2po]]
             [c2po.livereload-server :refer [render!]]
-
+            
             [c2po.geom :as geom]
             [c2po.stat :as stat]
             [c2po.group :as group]
+            [c2po.scale :as scale]
 
             [vomnibus.r :refer [mtcars]]
             [clojure.pprint :refer [pprint]]))
@@ -18,7 +19,7 @@
 
 ;;The c2po function sends this spec to the public c2po compiler and returns an svg string:
 (c2po scatterplot-spec) ;;"<svg height=\"660\" width=\"660\" ...
-;;If your company has an internally-hosted version of the compiler, you can pass its URL as the second argument.
+;;If your company has an internally-hosted version of the compiler, you can pass its URL as the second argument or call `(c2po.core/set-compiler-url! URL)` to change the default.
 
 ;;Getting a raw SVG string back isn't very convenient, so to aid interactive data visualization this client contains a livereload web server.
 ;;Calling
@@ -26,7 +27,7 @@
 (render! scatterplot-spec)
 
 ;;automatically starts a webserver on http://localhost:8987 and renders the provided spec.
-;;(If you need to use another port, you can start the server manually via (c2po.livereload-server/start-server PORT) before calling the render! function.)
+;;(To use another port, start the server manually via `(c2po.livereload-server/start-server PORT)` before calling the `render!`.)
 ;;Additional calls to render! will automatically update the plot in your browser---no need to refresh.
 
 ;;c2po has smart defaults, but you can easily override them by using records in your plot specifications instead of keywords.
@@ -38,8 +39,50 @@
   :mapping {:x :wt
             :y :mpg}})
 
-;;Note that we are working entirely with data structures, so we could have just as well written:
+;;Note that we are working entirely with data structures.
+;;Instead of writing out a new spec, we could have derived it from the original one by replacing the `:point` keyword with the more detailed record returned by the `(geom/point :radius 3)` constructor:
 
 (render!
  (assoc scatterplot-spec
    :geom (geom/point :radius 3)))
+
+;;Instead of giving the radius aesthetic a fixed value, we can also map it to a dimension of the data.
+;;The cars in this dataset all have
+
+(distinct (map :cyl mtcars)) ;;=> (6 4 8)
+
+;;cylinders in their engines.
+;;We can map this directly to points with 4, 6, and 8 pixels;
+
+(render!
+  {:data mtcars
+   :geom :point 
+   :mapping {:x :wt :y :mpg
+             :radius :cyl}})
+
+;;or manually specify the radial scale (a map of cyl -> pixel radius) to make the points more visually separable.
+
+(render!
+  {:data mtcars
+   :geom :point 
+   :mapping {:x :wt :y :mpg
+             :radius :cyl}
+   :scales {:radius {4 2, 6 6, 8 12}}})
+
+;;Some of the larger circles could be obscuring smaller ones, so a better choice might be to make the points semi-transparent, `(geom/point :opacity 0.5)`, or to map the number of cylinders to fill color instead of radius:
+
+(render!
+  {:data mtcars
+   :geom (geom/point :radius 8)
+   :mapping {:x :wt :y :mpg
+             :fill :cyl}
+   :scales {:fill {4 "hsl(0,0%,90%)"
+                   6 "hsl(0,0%,50%)"
+                   8 "hsl(0,0%,20%)"}}})
+
+;;By default the x and y scales of a plot are linear scales that cover the range of the data.
+;;We can explicitly provide  scales to control the plot's domains, ranges, and labels.
+
+(render! (assoc scatterplot-spec
+           :scales {:x (scale/linear :domain [0 10] :label "Weight" :ticks (range 11))
+                    :y (scale/linear :domain [10 40] :label "Miles per gallon")}))
