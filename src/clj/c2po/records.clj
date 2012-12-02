7bfbd9569a33ff02c77b9a33a50b0e2d2e5db2ea
Kevin J. Lynagh <kevin@keminglabs.com>
2012-12-01 16:44:24 -0800

    Pretty-print c2po records readably.

--- src/clj/c2po/records.clj
+++ src/clj/c2po/records.clj
@@ -1,16 +1,20 @@
 (ns c2po.records
   (:use [c2po.literals :only [print-literal]])
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            clojure.pprint))
 
 (defmacro def-c2po-record
   [prefix name]
   (let [record-name (str/capitalize (clojure.core/name name))]
     `(do
        (defrecord ~(symbol record-name) [])
-       ;;print the record appropriately
+
+       ;;print the record appropriately with Clojure's regular and pretty print systems
        (defmethod print-method ~(symbol record-name) [x# w#]
          (print-literal ~prefix ~(clojure.core/name name) x# w#))
        
+       (defmethod clojure.pprint/simple-dispatch ~(symbol record-name) [x#] (pr x#))
+
        ;;record constructor
        (defn ~name [& kwargs#]
          (~(symbol (str "map->" record-name))
