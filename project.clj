3b36b30d0fc151c5bca64608375372c01ee43053
Kevin J. Lynagh <kevin@keminglabs.com>
2012-12-24 14:49:35 +1300

    Include all deps in JAR until we can figure out a smart leiningen-profiles solution.

--- project.clj
+++ project.clj
@@ -2,16 +2,19 @@
   :description "Free-to-use client for the C2PO grammar of graphics"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-http "0.5.8"]
-                 [slingshot "0.10.3"]]
-  
-  :profiles {:dev {:dependencies [
-                                  ;;Bits for the livereload server
-                                  [compojure "1.1.3" :exclusions [org.clojure/tools.macro]]
-                                  [aleph "0.3.0-beta8" :exclusions [cheshire]]
-                                  [hiccup "1.0.2"]
+                 [slingshot "0.10.3"]
 
-                                  ;;Vomnibus is a collection of sample data sets
-                                  [com.keminglabs/vomnibus "0.3.2"]]}}
-  
+                 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                 ;;Can we factor out the below deps so that they can
+                 ;;be *optionally* sucked in downstream?
+                 
+                 ;;Bits for the livereload server
+                 [compojure "1.1.3" :exclusions [org.clojure/tools.macro]]
+                 [aleph "0.3.0-beta8" :exclusions [cheshire]]
+                 [hiccup "1.0.2"]
+
+                 ;;Vomnibus is a collection of sample data sets
+                 [com.keminglabs/vomnibus "0.3.2"]]
+
   :min-lein-version "2.0.0"
   :source-paths ["src/clj" "src/cljs"])
